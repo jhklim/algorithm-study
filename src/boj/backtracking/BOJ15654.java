c7752e0fdefc3fc2809ace97d72e8130724c6298
jaehyucklim <jhlim087@gmail.com>
2025-12-31 23:48:00 +0900

    feat: solve BOJ 15652 using backtracking

---
+++ src/boj/backtracking/BOJ15654.java
@@ -0,0 +1,55 @@
+package boj.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15654 {
+    static int N, M;
+    static int[] nums;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        arr = new int[M];
+        visit = new boolean[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[depth] = nums[i];
+                dfs(depth+1);
+                visit[i] = false;
+            }
+        }
+    }
+}
