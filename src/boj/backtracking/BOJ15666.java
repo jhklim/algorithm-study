94057ae42e289d9904bf4315ff9346e7eefd7f6c
jaehyucklim <jhlim087@gmail.com>
2026-01-03 22:58:53 +0900

    feat: solve BOJ 15666 using backtracking

---
+++ src/boj/backtracking/BOJ15666.java
@@ -0,0 +1,53 @@
+package boj.backtracking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15666 {
+    static int N, M;
+    static int[] nums;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nums = new int[N];
+        arr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+
+    private static void dfs(int depth, int last) {
+        if(depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(arr[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        int prev = -1;
+        for (int i = last; i < N; i++) {
+            if(prev != nums[i]) {
+                prev = nums[i];
+                arr[depth] = nums[i];
+                dfs(depth+1, i);
+            }
+        }
+    }
+}
