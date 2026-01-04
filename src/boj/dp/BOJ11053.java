31a09041b50d36290b758e65687988c7e6880614
jaehyucklim <jhlim087@gmail.com>
2026-01-04 17:01:00 +0900

    feat: solve BOJ 11053 using dp

---
+++ src/boj/dp/BOJ11053.java
@@ -0,0 +1,39 @@
+package boj.dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ11053 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i; j++) {
+                if(A[i] > A[j] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int max = -1;
+        for (int i = 0; i < N; i++) {
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
