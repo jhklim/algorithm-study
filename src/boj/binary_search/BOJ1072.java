989a4e24cc96226623468c8fbc3431fcd2d8cdf4
jaehyucklim <jhlim087@gmail.com>
2026-01-20 01:10:59 +0900

    feat: solve BOJ 1072 using binary search

---
+++ src/boj/binary_search/BOJ1072.java
@@ -0,0 +1,30 @@
+package boj.binary_search;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1072 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long X = Long.parseLong(st.nextToken());
+        long Y = Long.parseLong(st.nextToken());
+
+        long low = 1;
+        long high = (long)1e9;
+        long ans = -1;
+        while(low <= high) {
+            long mid = (low+high)/2;
+            if((Y*100)/X < ((Y+mid)*100)/(X+mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
