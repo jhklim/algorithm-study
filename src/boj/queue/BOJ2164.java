95359393cd315fd1fcd4c5c6ca1732f1ffe25553
jaehyucklim <jhlim087@gmail.com>
2026-01-22 16:18:37 +0900

    feat: solve BOJ 2164 using Queue

---
+++ src/boj/queue/BOJ2164.java
@@ -0,0 +1,25 @@
+package boj.queue;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ2164 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i < N+1; i++) {
+            queue.add(i);
+        }
+
+        while(queue.size() > 1) {
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.peek());
+    }
+}
